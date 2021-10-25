616b6b2f691669bfa449e059e2cf994aa8346374
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-10-25 18:06:33 -0400

    deploy a modulefile

--- build.sh
+++ build.sh
@@ -2,16 +2,13 @@
 
 set -eux
 
-target=${1:-${target:-"wcoss2"}}
-prefix=${2:-${prefix:-"../install"}}
-
-readlink=$(which readlink)
-[[ $(uname -s) == Darwin ]] && readlink=$(which greadlink)
+INSTALL_TARGET=${INSTALL_TARGET:-"wcoss2"}
+INSTALL_PREFIX=${INSTALL_PREFIX:-"../install"}
 
 # Location of PWD and package source directory.
-pkg_root=`dirname $(${readlink} -f $0)`
+pkg_root=`dirname $(readlink -f $0)`
 
-target=$(echo $target | tr [:upper:] [:lower:])
+target=$(echo $INSTALL_TARGET | tr [:upper:] [:lower:])
 if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
   source $pkg_root/versions/build.ver
   set +x
@@ -26,7 +23,7 @@ fi
 mkdir -p build && cd build
 
 # build and install.
-cmake -DCMAKE_INSTALL_PREFIX=$prefix ${CMAKE_OPTS:-} ..
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_INSTALL_BINDIR=exec ..
 make -j ${BUILD_JOBS:-6} VERBOSE=${BUILD_VERBOSE:-}
 make install
 
