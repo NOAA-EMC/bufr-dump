a8c05f91c63f744597c51ff9e2c2718c4589715f
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-10-25 11:55:40 -0400

    remove intel_ver as it is not used

--- modulefiles/bufrdump_wcoss2.lua
+++ modulefiles/bufrdump_wcoss2.lua
@@ -2,8 +2,6 @@ help([[
 Load environment to build bufr-dump on WCOSS2
 ]])
 
-intel_ver=os.getenv("intel_ver") or "default"
-
 load("envvar")
 load("PrgEnv-intel")
 load(pathJoin("intel/19.1.3.304"))
