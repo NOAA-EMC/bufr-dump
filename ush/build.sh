bd12e36f96794c5fac2e9a96b2eba8b2687debba
Iliana Genkova <iliana.genkova@noaa.gov>
2022-08-19 18:29:21 +0000

    Restore ush/build.sh and versions/VERSION

---
+++ ush/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eux
+
+# Location of PWD and package source directory.
+readonly pkg_root=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
+
+INSTALL_TARGET=${INSTALL_TARGET:-"wcoss2"}
+INSTALL_PREFIX=${INSTALL_PREFIX:-"$pkg_root/install"}
+MODULEFILE_INSTALL_PREFIX=${MODULEFILE_INSTALL_PREFIX:-"modulefiles"}
+
+target=$(echo $INSTALL_TARGET | tr [:upper:] [:lower:])
+if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
+  source $pkg_root/versions/build.ver
+  set +x
+  module purge
+  module use $pkg_root/modulefiles
+  module load bufrdump_$target
+  module list
+  set -x
+fi
+
+# Create a build directory and cd into it.
+[[ -d build  ]] && rm -rf build
+mkdir -p build && cd build
+
+# build and install.
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DCMAKE_INSTALL_BINDIR=exec \
+      -DMODULEFILE_INSTALL_PREFIX=$MODULEFILE_INSTALL_PREFIX \
+      ..
+make -j ${BUILD_JOBS:-6} VERBOSE=${BUILD_VERBOSE:-}
+make install
+
+# Remove build directory upon successfull build and install
+cd $pkg_root
+rm -rf build
+
+exit 0
