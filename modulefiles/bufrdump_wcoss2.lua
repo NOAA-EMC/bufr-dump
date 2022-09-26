d6c611c6a3390ef7dae9d5411dcb2178250e206a
Iliana Genkova <iliana.genkova@noaa.gov>
2022-09-26 23:07:10 +0000

    Fix readin versins in module

--- modulefiles/bufrdump_wcoss2.lua
+++ modulefiles/bufrdump_wcoss2.lua
@@ -2,6 +2,10 @@ help([[
 Load environment to build bufr-dump on WCOSS2
 ]])
 
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "default"
+intel_ver=os.getenv("intel_ver") or "default"
+cmake_ver=os.getenv("cmake_ver") or "default"
+
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("cmake", cmake_ver))
