d045530010dafebcce4c73f684064c00a9250ed9
Iliana Genkova <iliana.genkova@noaa.gov>
2022-09-26 22:55:17 +0000

    Fix ISERPRM call, cat<<eof|time syntax, add versions to module load

--- modulefiles/bufrdump_wcoss2.lua
+++ modulefiles/bufrdump_wcoss2.lua
@@ -2,11 +2,9 @@ help([[
 Load environment to build bufr-dump on WCOSS2
 ]])
 
-load("envvar")
-load("PrgEnv-intel")
-load("intel/19.1.3.304")
-load("cmake/3.20.2")
-
+load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
+load(pathJoin("intel", intel_ver))
+load(pathJoin("cmake", cmake_ver))
 
 -- Load common modules for this package
 load("bufrdump_common")
