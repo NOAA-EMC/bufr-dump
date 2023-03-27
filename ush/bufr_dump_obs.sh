5254ccf8d35dcd132f97e77246a35fed58386f85
Iliana Genkova <iliana.genkova@noaa.gov>
2023-03-27 20:46:48 +0000

    Remove duplicate gmi1cr, replace mv with cpfs, set VERSION 1.2

--- ush/bufr_dump_obs.sh
+++ ush/bufr_dump_obs.sh
@@ -1542,7 +1542,8 @@ do
    rm  $DATA/${n}.out
    if [ -s $DATA/${n}.${FORM} ];then
 ######cp  $DATA/${n}.${FORM} ${COMSP}${n}.${tmmark}.bufr_d
-      mv  $DATA/${n}.${FORM} ${COMSP}${n}.${tmmark}.bufr_d
+      # mv  $DATA/${n}.${FORM} ${COMSP}${n}.${tmmark}.bufr_d
+      cpfs $DATA/${n}.${FORM} ${COMSP}${n}.${tmmark}.bufr_d #NCO bugzilla Diane Stokes
       errmvt=$?
 ######rm  $DATA/${n}.${FORM}
       errmvl=$errmv
