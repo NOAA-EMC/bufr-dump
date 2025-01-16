bf17173ae4604a30c6fdad97c881b2a76e9fd263
Iliana Genkova <iliana.genkova@noaa.gov>
2025-01-16 20:35:17 +0000

    Set sofarw as "rstprod"

--- ush/bufr_dump_obs.sh
+++ ush/bufr_dump_obs.sh
@@ -1568,7 +1568,7 @@ do
    if [ "$CHGRP_RSTPROD" = 'YES' ]; then
       if [ $n = adpsfc -o $n = aircar -o $n = aircft -o $n = msonet -o \
            $n = sfcshp -o $n = lghtng -o $n = gpsipw -o $n = saphir -o \
-           $n = gpsro ]; then
+           $n = gpsro  -o $n = sofarw ]; then
          chgrp rstprod ${COMSP}${n}.${tmmark}.bufr_d
          errch=$?
          if [ $errch -eq 0 ]; then
